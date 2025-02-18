0f1a8518da5e4cbe6a0fee8696e9d4f36649a7fb
hekkort <harryvanderknaap@gmail.com>
2025-02-18 10:21:48 +0100

    adding asteroids

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+import constants
+
+def main():
+    pygame.init()
+    print("Starting asteroids!")
+    print("Screen width: " + str(constants.SCREEN_WIDTH))
+    print("Screen height: " + str(constants.SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()

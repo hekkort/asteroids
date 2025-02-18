4ba1c56b5be59c149c9604a9aa1fd51afd8d61c9
hekkort <harryvanderknaap@gmail.com>
2025-02-18 10:35:33 +0100

    more stuff added

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import constants
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting asteroids!")
     print("Screen width: " + str(constants.SCREEN_WIDTH))
     print("Screen height: " + str(constants.SCREEN_HEIGHT))
@@ -13,6 +15,8 @@ def main():
                 return
         screen.fill
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
